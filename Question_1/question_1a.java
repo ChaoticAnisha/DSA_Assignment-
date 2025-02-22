6f6960c53575f4dc491b3b5ae885e68a02e6c633
ChaoticAnisha <anishashah0117>
2025-02-22 12:29:44 +0545

    Question_1a few changes

--- Question_1/question_1a.java
+++ Question_1/question_1a.java
@@ -29,3 +29,11 @@ public class question_1a {
         System.out.println(findCriticalTemperature(3, 14));
     }
 }
+
+//Terminal output
+// PS D:\DESKTOP\DSA_Assignment> & 'C:\Program Files\Eclipse
+// Adoptium\jdk-17.0.11.9-hotspot\bin\java.exe'
+// '-XX:+ShowCodeDetailsInExceptionMessages' '-cp'
+// 'C:\Users\asus\AppData\Roaming\Code\User\workspaceStorage\ceb9750205844422ad612c526b471fbf\redhat.java\jdt_ws\DSA_Assignment_ecefc3e6\bin'
+// 'question_1a'
+//2 3 4
